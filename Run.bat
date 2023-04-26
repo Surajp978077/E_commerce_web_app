27ba66a506193a04edc185e3140aa22a8e348634
sp978077 <sp978077@gmail.com>
2023-04-26 15:31:40 +0530

    run.bat

--- Run.bat
+++ Run.bat
@@ -1,13 +1,11 @@
-start cmd /k "cd v1\login\Ecommerce && start https://localhost:7042 && dotnet run"
+start cmd /k "cd ..\EcommerceUsingBlazorDotNet\login\Ecommerce && start https://localhost:7042 && dotnet run"
 
-start cmd /k "cd v1\login\AuthJwtDbApi && dotnet run"
+start cmd /k "cd ..\EcommerceUsingBlazorDotNet\login\AuthJwtDbApi && dotnet run"
 
-start cmd /k "cd v1\login\Auth && dotnet run"
+start cmd /k "cd ..\EcommerceUsingBlazorDotNet\login\Auth && dotnet run"
 
-start cmd /k "cd v1\product\JwtDbApi && dotnet run"
-
-start cmd /k "cd v1\product\ConsumeJwtDbApi && dotnet run"
-
+start cmd /k "cd ..\EcommerceUsingBlazorDotNet\product\JwtDbApi && dotnet run"
+..\EcommerceUsingBlazorDotNet
 start cmd /k "cd Vendor && timeout /t 10 && npm start"
 
 start cmd /k "cd Admin && timeout /t 10 && npm start"
