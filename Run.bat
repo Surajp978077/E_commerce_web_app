8b2e160c37f7c25734f225628e172a1c5e42f0a0
sp978077 <sp978077@gmail.com>
2023-04-26 17:29:58 +0530

    modified bat file

--- Run.bat
+++ Run.bat
@@ -1,14 +1,7 @@
 start cmd /k "cd ..\EcommerceUsingBlazorDotNet\login\Ecommerce && start https://localhost:7042 && dotnet run"
-
- timeout /t 5
- 
+timeout /t 5 /nobreak
 start cmd /k "cd ..\EcommerceUsingBlazorDotNet\login\AuthJwtDbApi && dotnet run"
-
 start cmd /k "cd ..\EcommerceUsingBlazorDotNet\login\Auth && dotnet run"
-
 start cmd /k "cd ..\EcommerceUsingBlazorDotNet\product\JwtDbApi && dotnet run"
-
 start cmd /k "cd Vendor && npm start"
-
 start cmd /k "cd Admin && npm start"
-
