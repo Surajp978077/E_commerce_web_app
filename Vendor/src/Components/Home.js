72b5210893c17c5f725af77ed7541d69b41bc24e
sp978077 <sp978077@gmail.com>
2023-04-28 11:30:11 +0530

    vendor information get api endpoint

--- Vendor/src/Components/Home.js
+++ Vendor/src/Components/Home.js
@@ -1,11 +1,44 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react';
 
-const Home = () => {
-    return (
+function Home() {
+  const [vendor, setVendor] = useState(null);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const getVendorInfo = async (id) => {
+    try {
+      const response = await axios.get(`https://localhost:7044/api/Vendor/${id}`);
+      setVendor(response.data);
+      console.log(vendor)
+      setErrorMessage('');
+    } catch (error) {
+      setVendor(null);
+      setErrorMessage('Vendor not found');
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        const id = e.target.elements.vendorId.value;
+        getVendorInfo(id);
+      }}>
+        <input type="text" name="vendorId" placeholder="Enter vendor ID" />
+        <button type="submit">Get Vendor Info</button>
+      </form>
+      {vendor ? (
         <div>
-            <h1 >This is Vendors page</h1>
+          <h2>Vendor Info</h2>
+          <p>Name: {vendor.name}</p>
+          <p>GSTIN: {vendor.GSTIN}</p>
+          <p>DeliveryPinCode: {vendor.DeliveryPinCode}</p>
         </div>
-    )
+      ) : (
+        <p>{errorMessage}</p>
+      )}
+    </div>
+  );
 }
 
-export default Home
+export default Home;
