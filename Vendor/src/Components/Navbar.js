25090c087ea8a99808098619b4b7bb9968c59803
sp978077 <sp978077@gmail.com>
2023-05-25 02:45:39 +0530

    Changing Navbar.........

--- Vendor/src/Components/Navbar.js
+++ Vendor/src/Components/Navbar.js
@@ -1,139 +1,139 @@
-import React, { useState } from "react";
-import Offcanvas from "react-bootstrap/Offcanvas";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import Form from "react-bootstrap/Form";
-import { Link } from "react-router-dom";
-import jwtDecode from "jwt-decode";
-import { LOGINPAGE } from "../config/config";
-import {
-  Button,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemText,
-} from "@mui/material";
-import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+// import React, { useState } from "react";
+// import Offcanvas from "react-bootstrap/Offcanvas";
+// import Container from "react-bootstrap/Container";
+// import Nav from "react-bootstrap/Nav";
+// import Navbar from "react-bootstrap/Navbar";
+// import Form from "react-bootstrap/Form";
+// import { Link } from "react-router-dom";
+// import jwtDecode from "jwt-decode";
+// import { LOGINPAGE } from "../config/config";
+// import {
+//   Button,
+//   List,
+//   ListItem,
+//   ListItemButton,
+//   ListItemText,
+// } from "@mui/material";
+// import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
 
-export default function NavBar() {
-  const [show, setShow] = useState(false);
+// export default function NavBar() {
+//   const [show, setShow] = useState(false);
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+//   const handleClose = () => setShow(false);
+//   const handleShow = () => setShow(true);
 
-  var token = localStorage.getItem("token");
-  const decodedToken = jwtDecode(token);
+//   var token = localStorage.getItem("token");
+//   const decodedToken = jwtDecode(token);
 
-  function logout() {
-    localStorage.removeItem("token");
-    window.location.href = LOGINPAGE;
-  }
+//   function logout() {
+//  localStorage.removeItem("token");
+//     window.location.href = LOGINPAGE;
+//   }
 
-  return (
-    <>
-      <Navbar id="navbar" variant="light" sticky="top">
-        <Container fluid>
-          <Button
-            id="menu-collapse-button"
-            variant="outlined"
-            onClick={handleShow}
-          >
-            <MenuOutlinedIcon sx={{ color: "black" }} />
-          </Button>
-          <Link to={"/"} style={{ textDecoration: "none" }}>
-            <Navbar.Brand
-              onMouseOver={(e) => (e.target.style.color = "blue")}
-              onMouseOut={(e) => (e.target.style.color = "black")}
-            >
-              {" "}
-              E-commerce{" "}
-            </Navbar.Brand>
-          </Link>
-          <Nav className="me-auto">
-            {/* <Nav.Link href="#features">Features</Nav.Link> */}
-          </Nav>
-          <Link to={"/Profile"}>
-            <Button variant="outlined" color="success">
-              Welcome, {decodedToken.UserName}
-            </Button>
-          </Link>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="contained" sx={{ margin: "0 10px" }}>
-              Search
-            </Button>
-          </Form>
-          <Button color="error" variant="outlined" onClick={logout}>
-            Log-out
-          </Button>
-        </Container>
-      </Navbar>
+//   return (
+//     <>
+//       <Navbar id="navbar" variant="light" sticky="top">
+//         <Container fluid>
+//           <Button
+//             id="menu-collapse-button"
+//             variant="outlined"
+//             onClick={handleShow}
+//           >
+//             <MenuOutlinedIcon sx={{ color: "black" }} />
+//           </Button>
+//           <Link to={"/"} style={{ textDecoration: "none" }}>
+//             <Navbar.Brand
+//               onMouseOver={(e) => (e.target.style.color = "blue")}
+//               onMouseOut={(e) => (e.target.style.color = "black")}
+//             >
+//               {" "}
+//               E-commerce{" "}
+//             </Navbar.Brand>
+//           </Link>
+//           <Nav className="me-auto">
+//             {/* <Nav.Link href="#features">Features</Nav.Link> */}
+//           </Nav>
+//           <Link to={"/Profile"}>
+//             <Button variant="outlined" color="success">
+//               Welcome, {decodedToken.UserName}
+//             </Button>
+//           </Link>
+//           <Form className="d-flex">
+//             <Form.Control
+//               type="search"
+//               placeholder="Search"
+//               className="me-2"
+//               aria-label="Search"
+//             />
+//             <Button variant="contained" sx={{ margin: "0 10px" }}>
+//               Search
+//             </Button>
+//           </Form>
+//           <Button color="error" variant="outlined" onClick={logout}>
+//             Log-out
+//           </Button>
+//         </Container>
+//       </Navbar>
 
-      <Offcanvas
-        style={{ background: "linear-gradient(#2e005f, #4e0353)" }}
-        show={show}
-        onHide={handleClose}
-      >
-        <Offcanvas.Header closeButton>Ecommerce</Offcanvas.Header>
-        <Offcanvas.Body>
-          <List>
-            <ListItem disablePadding>
-              <ListItemButton>
-                <Link
-                  to={"/"}
-                  onClick={handleClose}
-                  style={{ textDecoration: "none" }}
-                >
-                  <ListItemText primary="Home" />
-                </Link>
-              </ListItemButton>
-            </ListItem>
-            <ListItem disablePadding>
-              <ListItemButton>
-                <Link
-                  to={"/Profile"}
-                  onClick={handleClose}
-                  style={{ textDecoration: "none" }}
-                >
-                  <ListItemText primary="Profile" />
-                </Link>
-              </ListItemButton>
-            </ListItem>
+//       <Offcanvas
+//         style={{ background: "linear-gradient(#2e005f, #4e0353)" }}
+//         show={show}
+//         onHide={handleClose}
+//       >
+//         <Offcanvas.Header closeButton>Ecommerce</Offcanvas.Header>
+//         <Offcanvas.Body>
+//           <List>
+//             <ListItem disablePadding>
+//               <ListItemButton>
+//                 <Link
+//                   to={"/"}
+//                   onClick={handleClose}
+//                   style={{ textDecoration: "none" }}
+//                 >
+//                   <ListItemText primary="Home" />
+//                 </Link>
+//               </ListItemButton>
+//             </ListItem>
+//             <ListItem disablePadding>
+//               <ListItemButton>
+//                 <Link
+//                   to={"/Profile"}
+//                   onClick={handleClose}
+//                   style={{ textDecoration: "none" }}
+//                 >
+//                   <ListItemText primary="Profile" />
+//                 </Link>
+//               </ListItemButton>
+//             </ListItem>
 
-            <ListItem disablePadding>
-              <ListItemButton>
-                <Link
-                  to={"/Listings"}
-                  onClick={handleClose}
-                  style={{ textDecoration: "none" }}
-                >
-                  Listings
-                </Link>
-              </ListItemButton>
-            </ListItem>
-            <ListItem disablePadding>
-              <ListItemButton>
-                <Link
-                  to={"/test"}
-                  onClick={handleClose}
-                  style={{ textDecoration: "none" }}
-                >
-                  <ListItemText primary="Test" />
-                </Link>
-              </ListItemButton>
-            </ListItem>
-          </List>
-        </Offcanvas.Body>
-      </Offcanvas>
-    </>
-  );
-}
+//             <ListItem disablePadding>
+//               <ListItemButton>
+//                 <Link
+//                   to={"/Listings"}
+//                   onClick={handleClose}
+//                   style={{ textDecoration: "none" }}
+//                 >
+//                   Listings
+//                 </Link>
+//               </ListItemButton>
+//             </ListItem>
+//             <ListItem disablePadding>
+//               <ListItemButton>
+//                 <Link
+//                   to={"/test"}
+//                   onClick={handleClose}
+//                   style={{ textDecoration: "none" }}
+//                 >
+//                   <ListItemText primary="Test" />
+//                 </Link>
+//               </ListItemButton>
+//             </ListItem>
+//           </List>
+//         </Offcanvas.Body>
+//       </Offcanvas>
+//     </>
+//   );
+// }
 
 // import * as React from "react";
 // import PropTypes from "prop-types";
@@ -287,3 +287,188 @@ export default function NavBar() {
 // }
 
 // export default ResponsiveDrawer;
+
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Button from "@mui/material/Button";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import MenuIcon from "@mui/icons-material/Menu";
+import { Outlet } from "react-router-dom";
+import { LOGO } from "../config/config";
+import { Link as RouterLink } from "react-router-dom";
+import ClearIcon from "@mui/icons-material/Clear";
+import { LOGINPAGE } from "../config/config";
+import {
+  Avatar,
+  IconButton,
+  Menu,
+  Typography,
+  MenuItem,
+  Tooltip,
+} from "@mui/material";
+import jwtDecode from "jwt-decode";
+
+const Link = React.forwardRef(function Link(itemProps, ref) {
+  return <RouterLink ref={ref} {...itemProps} role={undefined} />;
+});
+function ListItemLink(props) {
+  const { icon, primary, to } = props;
+
+  return (
+    <ListItem button component={Link} to={to}>
+      {icon ? <ListItemIcon>{icon}</ListItemIcon> : null}
+      <ListItemText primary={primary} />
+    </ListItem>
+  );
+}
+
+export default function Navbar() {
+  const anchor = "left";
+  const [state, setState] = React.useState({ [anchor]: false });
+
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+  var token = localStorage.getItem("token");
+  const decodedToken = jwtDecode(token);
+
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  function logout() {
+    localStorage.removeItem("token");
+    window.location.href = LOGINPAGE;
+  }
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setState((prevState) => ({
+      ...prevState,
+      [anchor]: open,
+    }));
+  };
+
+  const list = (anchor) => (
+    <Box
+      sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 350 }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <Toolbar id="offcanvas-header">
+        <IconButton sx={{ p: 0 }}>
+          <Avatar src={decodedToken.profilePic}></Avatar>
+        </IconButton>
+        <Typography sx={{ marginLeft: "10px" }}>
+          Hello, {decodedToken.UserName}
+        </Typography>
+        <ClearIcon
+          id="drawer-close-btn"
+          onClick={toggleDrawer(anchor, false)}
+        />
+      </Toolbar>
+      <Divider />
+      <List>
+        <ListItemLink to="/" primary="Home" />
+
+        <ListItemLink to="/Profile" primary="Profile" />
+
+        <ListItemLink to="/Listings" primary="Listings" />
+
+        <ListItemLink to="/Test" primary="Test" />
+      </List>
+    </Box>
+  );
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar
+        id="navbar"
+        position="sticky"
+        sx={{
+          flexDirection: "column",
+          justifyContent: "center",
+        }}
+      >
+        <Toolbar>
+          <React.Fragment key={anchor}>
+            <Button
+              onClick={toggleDrawer(anchor, true)}
+              sx={{ color: "black" }}
+            >
+              <MenuIcon />
+            </Button>
+            <Drawer
+              anchor={anchor}
+              open={state[anchor]}
+              onClose={toggleDrawer(anchor, false)}
+            >
+              {list("left")}
+            </Drawer>
+            <div component={RouterLink} to="/" className="header-title">
+              <img src={LOGO} className="header-logo" alt="header-Logo" />
+            </div>
+          </React.Fragment>
+          <Box sx={{ flexGrow: 1 }} />
+
+          <Tooltip title="Open account" sx={{ marginLeft: "auto" }}>
+            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+              <Avatar
+                alt={decodedToken.UserName.charAt(0)}
+                src={decodedToken.profilePic}
+              >
+                {decodedToken.UserName ? decodedToken.UserName.charAt(0) : ""}
+              </Avatar>
+            </IconButton>
+          </Tooltip>
+          <Menu
+            sx={{ mt: "45px" }}
+            id="menu-appbar"
+            anchorEl={anchorElUser}
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            open={Boolean(anchorElUser)}
+            onClose={handleCloseUserMenu}
+          >
+            <MenuItem
+              key="Profile"
+              component={RouterLink}
+              to="/Profile"
+              onClick={handleCloseUserMenu}
+            >
+              <Typography textAlign="center">Profile</Typography>
+            </MenuItem>
+            <MenuItem key="Logout" onClick={logout}>
+              <Typography color="error" textAlign="center">
+                Logout
+              </Typography>
+            </MenuItem>
+          </Menu>
+        </Toolbar>
+      </AppBar>
+      <Outlet />
+    </Box>
+  );
+}
